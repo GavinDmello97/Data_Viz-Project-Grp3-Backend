e0edadd9536e2b7ad86e08afe121aeb8e715ac76
Gavin D'mello <gavindmello@Gavins-MacBook-Pro.local>
2021-11-22 22:31:57 -0500

    added cors support

--- index.js
+++ index.js
@@ -45,6 +45,14 @@ app.get("/contributors", cors(),(req, res) => {
 });
 
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 const port = process.env.PORT || 3000 //First inititalizing the port
 
 app.listen(port,()=>  console.log(`Express is listening on port:${port}`))
